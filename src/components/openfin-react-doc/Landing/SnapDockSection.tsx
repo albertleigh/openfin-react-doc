7c2f63f82bf20a6d2a0a29529b23b3b71d334f64
Albert Li <liwentao90@yahoo.com>
2018-12-24 11:29:12 -0500

    - add abstractLandingSection class to isolate some logic out of snapDockSection comp

--- src/components/openfin-react-doc/Landing/SnapDockSection.tsx
+++ src/components/openfin-react-doc/Landing/SnapDockSection.tsx
@@ -4,6 +4,8 @@ import { WithStyles, withStyles } from '@material-ui/core/styles';
 
 import { landingSnapDockSectionCompStyle as style } from '../../../assets/jss/openfin-react-doc';
 
+import AbstractLandingSection from './AbstractLandingSection';
+
 interface IProps extends WithStyles<typeof style>{
 
 }
@@ -14,46 +16,25 @@ interface IState{
     // [key:string]:any,
 }
 
-class SnapDockSectionComp extends React.Component<IProps, IState>{
+class SnapDockSectionComp extends AbstractLandingSection<IProps, IState>{
 
     element:any;
-    observer:IntersectionObserver;
 
     state={
         visiblePct: 0,
     }
 
     componentDidMount(): void {
-        this.observer = new IntersectionObserver(
-            entries => {
-                console.log('SnapDockSectionComp::componentDidMount 1#',entries, this.element);
-                entries.forEach((entry)=>{
-                    console.log('SnapDockSectionComp::componentDidMount 2#',entry, this.element);
-                    if (entry.target === this.element){
-                        console.log('SnapDockSectionComp::componentDidMount 3#',entry.intersectionRatio);
-                        this.setState({
-                            visiblePct:Math.floor(entry.intersectionRatio*100)/100
-                        })
-                    }
-                })
-
-
-            },
-            {
-                root: document.querySelector(".landingContainer"),
-                rootMargin:'0px 0px 0px 0px',
-                threshold:[0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-            }
-        )
-        this.observer.observe(this.element);
+        super.componentDidMount();
     }
 
     componentWillUnmount(): void {
-        if (this.observer){
-            this.observer.disconnect();
-        }
+        super.componentWillUnmount();
     }
 
+    onIntersectionChanged =(intersectionObserverEntry:IntersectionObserverEntry)=>{
+        console.log("SnapDockSectionComp::onIntersectionChanged",intersectionObserverEntry);
+    }
 
     render(){
 
