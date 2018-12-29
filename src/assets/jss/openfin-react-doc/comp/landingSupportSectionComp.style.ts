03e0d3caed4a9c8a76a8a02e5f8f425ab9d8fbce
Albert Li <liwentao90@yahoo.com>
2018-12-29 01:27:07 -0500

    - initially settle support section at landing lyt 2#

--- src/assets/jss/openfin-react-doc/comp/landingSupportSectionComp.style.ts
+++ src/assets/jss/openfin-react-doc/comp/landingSupportSectionComp.style.ts
@@ -5,11 +5,37 @@ const landingSupportSectionCompStyle = (theme:Theme)=> createStyles({
         width: '100%',
         height: '100%',
         padding:'0 10% 0 10%',
+        [theme.breakpoints.up('lg')]: {
+            display:'flex',
+            flexWrap:'nowrap',
+            justifyContent:'space-around',
+        },
+    },
+
+    oneSubSec:{
+        "& >:not(:nth-child(1))":{
+            marginLeft:theme.spacing.unit,
+        },
     },
 
     header:{
         marginTop:10,
-    }
+    },
+
+    flatIconCreditContainers:{
+      display:'flex',
+      flexWrap:'nowrap',
+    },
+    flatIconCreditAvatar:{
+        width:20,
+        height:20,
+        marginRight:theme.spacing.unit/2,
+    },
+
+    supporterAnchor:{
+        marginRight:theme.spacing.unit*2,
+    },
+
 });
 
 export default landingSupportSectionCompStyle;
--- src/components/openfin-react-doc/Landing/SupportSection.tsx
+++ src/components/openfin-react-doc/Landing/SupportSection.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames';
 import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 import { withNamespaces, WithNamespaces } from 'react-i18next';
@@ -11,6 +12,17 @@ import { withNamespaces, WithNamespaces } from 'react-i18next';
 import { landingSupportSectionCompStyle as style } from '../../../assets/jss/openfin-react-doc';
 
 import AbstractLandingSection from "./AbstractLandingSection";
+
+import dbGearsSvg from '../../../assets/svg/developer/database-gears.svg';
+import githubSvg from '../../../assets/svg/developer/github-logo.svg';
+import githubDarkSvg from '../../../assets/svg/developer/github-logo-dark.svg';
+import chinaFlagSvg from '../../../assets/svg/language/china.svg';
+import usFlagSvg from '../../../assets/svg/language/united-states.svg';
+import settingSvg from '../../../assets/svg/other/settings.svg';
+
+import openfinLogoSvg from '../../../assets/svg/developer/openfin.svg';
+import matUiLogoSvg from '../../../assets/svg/developer/material-ui-logo.svg';
+import reactSvg from '../../../assets/svg/developer/react.svg';
 
 interface IProps extends WithStyles<typeof style>, WithNamespaces{
     onIntersectionChanged: (intersectionObserverEntry:IntersectionObserverEntry) =>void,
@@ -22,15 +34,31 @@ interface IContributor{
     email:string,
 }
 
+interface IFlatIconCredit{
+    icons:string[],
+    authorName:string,
+    authorLink:string,
+}
+
+interface ISupporter{
+    name:string,
+    icon:string,
+    link:string,
+    width:string|number,
+    height:string|number,
+}
+
 interface IState{
     contributors:IContributor[],
+    flatIconCredits:IFlatIconCredit[],
+    supporters:ISupporter[],
 }
 
 class SupportSectionComp extends AbstractLandingSection<IProps, IState>{
 
     element:any;
 
-    state={
+    state:IState={
         visiblePct: 0,
         contributors:[
             {
@@ -39,7 +67,46 @@ class SupportSectionComp extends AbstractLandingSection<IProps, IState>{
                 email:'liwentao90@yahoo.com',
             }
         ],
-    }
+        flatIconCredits:[
+            {
+                icons:[
+                    githubSvg,githubDarkSvg,chinaFlagSvg,usFlagSvg,
+                ],
+                authorName:'Freepik',
+                authorLink:'http://www.freepik.com/',
+            },
+            {
+                icons:[
+                    dbGearsSvg,settingSvg,
+                ],
+                authorName:'Smashicons',
+                authorLink:'https://smashicons.com/',
+            },
+        ],
+        supporters:[
+            {
+                name:'Openfin',
+                icon:openfinLogoSvg,
+                link:'https://openfin.co/',
+                height:'3.626em',
+                width:'10em',
+            },
+            {
+                name:'Material UI',
+                icon:matUiLogoSvg,
+                link:'https://material-ui.com/',
+                height:'3.626em',
+                width:'auto',
+            },
+            {
+                name:'React',
+                icon:reactSvg,
+                link:'https://reactjs.org/',
+                height:'3.626em',
+                width:'auto',
+            },
+        ],
+    } as IState;
 
     componentDidMount(): void {
         super.componentDidMount();
@@ -64,45 +131,74 @@ class SupportSectionComp extends AbstractLandingSection<IProps, IState>{
     render(){
 
         const { classes, t } = this.props;
-        const { contributors }= this.state;
+        const { contributors, flatIconCredits, supporters }= this.state;
 
         return(
             <div
                 className={classes.container}
                 ref = {el => this.element = el}
             >
-                <Typography className={classes.header} variant="h5" gutterBottom>
-                    Contributor:
-                </Typography>
-                {
-                    contributors.map(((contributor:IContributor,index,arr)=>(
-                        <Chip
-                            key={index}
-                            avatar={<Avatar alt={contributor.name} src={contributor.avatar} />}
-                            label={contributor.name}
-                            onClick={this.handleEmailBtnClick(contributor.email)}
-                        />
-                    )))
+                <div className={classes.oneSubSec}>
+                    <Typography className={classes.header} variant="h5" gutterBottom>
+                        Contributor:
+                    </Typography>
+                    {
+                        contributors.map(((contributor:IContributor,index,arr)=>(
+                            <Chip
+                                key={index}
+                                avatar={<Avatar alt={contributor.name} src={contributor.avatar} />}
+                                label={contributor.name}
+                                onClick={this.handleEmailBtnClick(contributor.email)}
+                            />
+                        )))
 
-                }
+                    }
+                </div>
 
-                <Typography className={classes.header} variant="h5" gutterBottom>
-                    Credits:
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                    unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-                    dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-                </Typography>
+                <div className={classes.oneSubSec}>
+                    <Typography className={classes.header} variant="h5" gutterBottom>
+                        Credits:
+                    </Typography>
+                    {
+                        flatIconCredits.map((oneFlatIconCredit:IFlatIconCredit, flatIconIndex, flatIconArr)=>(
+                            <div className={classes.flatIconCreditContainers} key={flatIconIndex}>
+                                {oneFlatIconCredit.icons.map((icon:string, iconIndex, iconArr) => (
+                                    <Avatar
+                                        className = {classes.flatIconCreditAvatar}
+                                        key={iconIndex} src={icon} />
+                                ))}
+                                <Typography variant="body1" gutterBottom>
+                                    Icon made by <a href={oneFlatIconCredit.authorLink}>{oneFlatIconCredit.authorName}</a> from <a href="http://www.flaticon.com">www.flaticon.com</a>
+                                </Typography>
+                            </div>
+                        ))
 
-                <Typography className={classes.header} variant="h5" gutterBottom>
-                    Support:
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-                    unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-                    dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-                </Typography>
+                    }
+                </div>
+
+                <div className={classes.oneSubSec}>
+                    <Typography className={classes.header} variant="h5" gutterBottom>
+                        Support:
+                    </Typography>
+                    <div>
+                        {
+                            supporters.map((supporter:ISupporter,index,arr)=>(
+                                <a className={classes.supporterAnchor}
+                                   key={index} href={supporter.link}>
+                                    <Tooltip title={supporter.name}>
+                                        <img alt={supporter.name} src={supporter.icon}
+                                             style={{
+                                                 'width': supporter.width,
+                                                 'height': supporter.height,
+                                             }}
+                                        />
+                                    </Tooltip>
+                                </a>
+                            ))
+
+                        }
+                    </div>
+                </div>
             </div>
         )
     }
