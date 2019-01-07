0df9ab95e7f211ecb2a7a17ba815bc1db6941f7e
Albert Li <liwentao90@yahoo.com>
2019-01-07 11:05:21 -0500

    - initially populating startingInstallation 5#

--- src/assets/jss/openfin-react-doc/view/startInstallationView.style.ts
+++ src/assets/jss/openfin-react-doc/view/startInstallationView.style.ts
@@ -2,6 +2,20 @@ import { Theme, createStyles } from '@material-ui/core/styles';
 
 const startInstallationViewStyle = (theme:Theme)=> createStyles({
 
+    sampleInstallerContainer:{
+        display:'flex',
+        flexWrap:'nowrap',
+        justifyContent:'flex-start',
+        alignItems:'baseline',
+        "& >:nth-child(1)":{
+            marginRight: theme.spacing.unit,
+        },
+    },
+    anchor:{
+
+        color:theme.palette.text.primary
+    }
+
 });
 
 export default startInstallationViewStyle;
--- src/views/Start/StartInstallation.tsx
+++ src/views/Start/StartInstallation.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 
-import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import SettingIcon from '@material-ui/icons/Settings';
 
 import cx from "classnames";
 import { connect } from 'react-redux';
@@ -19,15 +22,54 @@ interface IProps extends WithStyles<typeof style>{
 import { IRootState } from '../../reduxs';
 
 class StartInstallationView extends React.Component<IProps,{}>{
+
+    handleSampleInstallerClick = ()=>{
+        window.open("https://install.openfin.co/download/?config=https%3A%2F%2Fwinslow90.github.io%2Fapp.json&fileName=openfin-react-installer&supportEmail=liwentao90%40yahoo.com",'_blank');
+    }
+
     render(){
+
+        const {classes} = this.props;
+
         return (
             <React.Fragment>
                 <Typography variant='h2' gutterBottom>
-                    Installation & Setup
+                    Installer
+                </Typography>
+                <Typography variant='h4' gutterBottom>
+                    Windows
+                </Typography>
+
+                <div className={classes.sampleInstallerContainer}>
+                    <Typography variant='h6' gutterBottom>
+                        Sample installer
+                    </Typography>
+                    <IconButton aria-label="SampleInstaller"
+                                onClick={this.handleSampleInstallerClick}
+                    >
+                        <SettingIcon fontSize="small" />
+                    </IconButton>
+                </div>
+
+                <Typography variant="body1" gutterBottom>
+                    Openfin provides an window platform installer generator to generate installer of your application.
                 </Typography>
                 <Typography variant="body1" gutterBottom>
-                    Initially, install the scaffolding tool to generate template.
+                    <a className={classes.anchor}
+                       href='https://install.openfin.co/'>https://install.openfin.co</a>
                 </Typography>
+
+                <Typography variant='h4' gutterBottom>
+                    Mac & Linux
+                </Typography>
+                <Typography variant="caption" gutterBottom>
+                    Current installers upon mac and linux platform are not supported by Openfin.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    But an alternative solution is provided via al-cli.
+                    Plz check scaffolding al-cli section.
+                </Typography>
+
 
             </React.Fragment>
         )
