60a2e948dd062a9d0a6f78d0b10e4216fd2606e7
Albert Li <liwentao90@yahoo.com>
2019-01-07 01:06:49 -0500

    - initially populating startingInstallation 4#

--- src/views/Scaffolding/ScaffoldingInstall.tsx
+++ src/views/Scaffolding/ScaffoldingInstall.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
 import cx from "classnames";
 import { connect } from 'react-redux';
 
 import { scaffoldingInstallViewStyle as style } from '../../assets/jss/openfin-react-doc';
+
+import { Code } from '../../components';
 
 interface IProps extends WithStyles<typeof style>{
 
@@ -12,11 +18,51 @@ interface IProps extends WithStyles<typeof style>{
 
 import { IRootState } from '../../reduxs';
 
+const alCli =
+    `
+        _ _               _             _ _ 
+   __ _| | |__   ___ _ __| |_       ___| (_)
+  / _\` | | '_ \\ / _ \\ '__| __|____ / __| | |
+ | (_| | | |_) |  __/ |  | ||_____| (__| | |
+  \\__,_|_|_.__/ \\___|_|   \\__|     \\___|_|_|
+                                            
+  v0.30.10
+ 
+  albert li personal cli to generate boilerplate codes
+ 
+  SYNOPSIS
+      $ al-cli comp <component name>
+      $ al-cli openfin-react-ts <app name>
+      $ al-cli react-ts <app name>
+      $ al-cli help
+  DESCRIPTION
+      --force -f          Create the target file by force, override if any already exist
+      --directory -d      Override the default path where the file created
+      --verbose -v        Include comments
+      --skipInstall       Skip to install node_moduels
+`;
+
 class ScaffoldingInstallView extends React.Component<IProps,{}>{
     render(){
         return (
             <React.Fragment>
-                ScaffoldingInstall view works ~
+                <Typography variant='h2' gutterBottom>
+                    Installation & Setup
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    Initially, install the scaffolding tool to generate template.
+                </Typography>
+                <Code withMargin text="npm install @albertli90/al-cli -g"/>
+
+                <Typography variant="body1" gutterBottom>
+                    or
+                </Typography>
+                <Code withMargin text="yarn global add @albertli90/al-cli"/>
+
+                <Paper>
+                    <Code language='vim' withMargin text={alCli}/>
+                </Paper>
+
             </React.Fragment>
         )
     }
--- src/views/Start/StartInstallation.tsx
+++ src/views/Start/StartInstallation.tsx
@@ -18,30 +18,6 @@ interface IProps extends WithStyles<typeof style>{
 
 import { IRootState } from '../../reduxs';
 
-const alCli =
-`
-        _ _               _             _ _ 
-   __ _| | |__   ___ _ __| |_       ___| (_)
-  / _\` | | '_ \\ / _ \\ '__| __|____ / __| | |
- | (_| | | |_) |  __/ |  | ||_____| (__| | |
-  \\__,_|_|_.__/ \\___|_|   \\__|     \\___|_|_|
-                                            
-  v0.30.10
- 
-  albert li personal cli to generate boilerplate codes
- 
-  SYNOPSIS
-      $ al-cli comp <component name>
-      $ al-cli openfin-react-ts <app name>
-      $ al-cli react-ts <app name>
-      $ al-cli help
-  DESCRIPTION
-      --force -f          Create the target file by force, override if any already exist
-      --directory -d      Override the default path where the file created
-      --verbose -v        Include comments
-      --skipInstall       Skip to install node_moduels
-`;
-
 class StartInstallationView extends React.Component<IProps,{}>{
     render(){
         return (
@@ -51,21 +27,6 @@ class StartInstallationView extends React.Component<IProps,{}>{
                 </Typography>
                 <Typography variant="body1" gutterBottom>
                     Initially, install the scaffolding tool to generate template.
-                </Typography>
-                <Code withMargin text="npm install @albertli90/al-cli -g"/>
-
-                <Typography variant="body1" gutterBottom>
-                    or
-                </Typography>
-                <Code withMargin text="yarn global add @albertli90/al-cli"/>
-
-
-                <Paper>
-                    <Code language='vim' withMargin text={alCli}/>
-                </Paper>
-
-                <Typography variant="body1" gutterBottom>
-                    test 3
                 </Typography>
 
             </React.Fragment>
