385319e592909f143894ca88870bbac92745bcf0
Albert Li <liwentao90@yahoo.com>
2019-01-08 05:30:32 -0500

    - initially populate start views

--- src/views/Start/StartDevelopers.tsx
+++ src/views/Start/StartDevelopers.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+
 import cx from "classnames";
 import { connect } from 'react-redux';
 
@@ -16,7 +19,12 @@ class StartDevelopersView extends React.Component<IProps,{}>{
     render(){
         return (
             <React.Fragment>
-                StartDevelopers view works ~
+                <Typography variant='h3' gutterBottom>
+                    Big Pic
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                    todo: to be finished
+                </Typography>
             </React.Fragment>
         )
     }
