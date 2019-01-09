b68d0c77423854810bdb35473075ade0dd27c772
Albert Li <liwentao90@yahoo.com>
2019-01-09 12:11:53 -0500

    - initially populating scaffoldingStandalonePkg view 1#

--- src/assets/jss/openfin-react-doc/view/scaffoldingStandalonePkgView.style.ts
+++ src/assets/jss/openfin-react-doc/view/scaffoldingStandalonePkgView.style.ts
@@ -1,7 +1,9 @@
 import { Theme, createStyles } from '@material-ui/core/styles';
 
 const scaffoldingStandalonePkgViewStyle = (theme:Theme)=> createStyles({
-
+    hasMargin:{
+        marginTop: theme.spacing.unit * 6,
+    },
 });
 
 export default scaffoldingStandalonePkgViewStyle;
