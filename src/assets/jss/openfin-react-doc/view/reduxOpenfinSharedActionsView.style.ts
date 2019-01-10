2b5d4b2433a4f60e521c2ef906b27b6c6daf38fb
Albert Li <liwentao90@yahoo.com>
2019-01-10 16:45:57 -0500

    - initially populating ReduxOpenfinSharedActionsView view 1#

--- src/assets/jss/openfin-react-doc/view/reduxOpenfinSharedActionsView.style.ts
+++ src/assets/jss/openfin-react-doc/view/reduxOpenfinSharedActionsView.style.ts
@@ -2,6 +2,10 @@ import { Theme, createStyles } from '@material-ui/core/styles';
 
 const reduxOpenfinSharedActionsViewStyle = (theme:Theme)=> createStyles({
 
+    hasMargin:{
+        marginTop: theme.spacing.unit * 6,
+    },
+
 });
 
 export default reduxOpenfinSharedActionsViewStyle;
--- src/views/ReduxOpenfin/ReduxOpenfinSharedActions.tsx
+++ src/views/ReduxOpenfin/ReduxOpenfinSharedActions.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
 import cx from "classnames";
 import { connect } from 'react-redux';
 
@@ -14,9 +16,30 @@ import { IRootState } from '../../reduxs';
 
 class ReduxOpenfinSharedActionsView extends React.Component<IProps,{}>{
     render(){
+
+        const { classes } = this.props;
+
         return (
             <React.Fragment>
-                ReduxOpenfinSharedActions view works ~
+                <Typography variant='h3' gutterBottom>
+                    Sharing actions
+                </Typography>
+                <Typography variant='subheading' gutterBottom>
+                    Cross-window communication via redux action
+                </Typography>
+
+                <Typography className={classes.hasMargin} variant='body1' gutterBottom>
+                    React.js usually is utilized to build SPA(single page application) webpages / stack of views mobile app.
+                    However desktop app usually calls for multi-windows features, even more usually,
+                    these windows need communicate with each other, which make developing Desktop app via SPA tricky.
+                </Typography>
+                <Typography variant='body1' gutterBottom>
+                    Therefore, redux-openfin provides a out-of-box solution to allow different windows belonging to the same SPA application
+                    share certain Redux Actions if needed. Since the lib is called redux-openfin, there ought not to much complains to directly use
+                    Redux actions as cross windows meta object.
+                </Typography>
+
+
             </React.Fragment>
         )
     }
