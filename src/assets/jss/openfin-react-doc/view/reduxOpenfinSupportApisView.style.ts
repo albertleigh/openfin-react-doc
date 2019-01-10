feddd49677c8815e99876724f9626a88884eb5f7
Albert Li <liwentao90@yahoo.com>
2019-01-10 15:29:42 -0500

    - initially populating ReduxOpenfinSupportApis view 1#

--- src/assets/jss/openfin-react-doc/view/reduxOpenfinSupportApisView.style.ts
+++ src/assets/jss/openfin-react-doc/view/reduxOpenfinSupportApisView.style.ts
@@ -2,6 +2,17 @@ import { Theme, createStyles } from '@material-ui/core/styles';
 
 const reduxOpenfinSupportApisViewStyle = (theme:Theme)=> createStyles({
 
+    outmostContainer:{
+        "& >h4":{
+            marginTop: theme.spacing.unit * 6,
+        },
+
+        "& >h5":{
+            marginTop: theme.spacing.unit * 4,
+            marginLeft: theme.spacing.unit * 2,
+        }
+    },
+
 });
 
 export default reduxOpenfinSupportApisViewStyle;
