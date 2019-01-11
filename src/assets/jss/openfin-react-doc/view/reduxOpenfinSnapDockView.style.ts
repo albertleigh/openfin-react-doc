a664c12b797057f20f9f00e06b6bf97ab56bd656
Albert Li <liwentao90@yahoo.com>
2019-01-11 12:29:03 -0500

    - initially populating ReduxOpenfinSnapDockView view 1#

--- src/assets/jss/openfin-react-doc/view/reduxOpenfinSnapDockView.style.ts
+++ src/assets/jss/openfin-react-doc/view/reduxOpenfinSnapDockView.style.ts
@@ -2,6 +2,10 @@ import { Theme, createStyles } from '@material-ui/core/styles';
 
 const reduxOpenfinSnapDockViewStyle = (theme:Theme)=> createStyles({
 
+    hasMargin:{
+        marginTop: theme.spacing.unit * 6,
+    },
+
 });
 
 export default reduxOpenfinSnapDockViewStyle;
--- src/views/ReduxOpenfin/ReduxOpenfinSnapDock.tsx
+++ src/views/ReduxOpenfin/ReduxOpenfinSnapDock.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
 import cx from "classnames";
 import { connect } from 'react-redux';
 
@@ -12,11 +14,54 @@ interface IProps extends WithStyles<typeof style>{
 
 import { IRootState } from '../../reduxs';
 
+import configureStoreCode from '!raw-loader!./samples/configureStore_ts';
+
+import { Code } from '../../components';
+
 class ReduxOpenfinSnapDockView extends React.Component<IProps,{}>{
     render(){
+
+        const { classes } = this.props;
+
         return (
             <React.Fragment>
-                ReduxOpenfinSnapDock view works ~
+                <Typography variant='h3' gutterBottom>
+                    Snap & Dock
+                </Typography>
+
+
+
+                <Typography variant='h5' gutterBottom>
+                    Configuration
+                </Typography>
+                <Typography variant='subtitle1' gutterBottom>
+                    Out of box snap&dock cross-windows feature is provided, which could be enabled or disabled via
+                    IConfig Object.
+                </Typography>
+
+                <Typography variant='caption' gutterBottom>
+                    Sample to create redux-openfin middleware
+                </Typography>
+                <Code withMargin text={configureStoreCode} />
+                <Typography variant='body1' gutterBottom>
+                    In the sample to create openfin middleware,
+                    the autoDocking set to be based on REACT_APP_ENABLE_AUTO_DOCKING is set or not.
+                    When autoDocking is set, the out of box snap&dock will be provided.
+                </Typography>
+
+
+
+
+
+                <Typography className={classes.hasMargin} variant='h5' gutterBottom>
+                    Ctrl+Shift+U / Command+Shift+U to undock all
+                </Typography>
+                <Typography variant='body1' gutterBottom>
+                    Another thing which might be help is be default, the ctrl+shift+u shortcut is listened to undock all.
+                    If needed, user can directly use this shortcut to undock all windows already docked no matter what.
+                </Typography>
+
+
             </React.Fragment>
         )
     }
