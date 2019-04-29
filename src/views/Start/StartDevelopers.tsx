38b479c18ac762a1eeaa4ae0eb1564a10c824d4a
Wentao LI <liwentao90@yahoo.com>
2019-04-29 13:47:32 -0400

    - refactor into funComponents with hook apis 7#

--- src/views/Start/StartDevelopers.tsx
+++ src/views/Start/StartDevelopers.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { WithStyles, withStyles } from '@material-ui/core/styles';
-import { withNamespaces, WithNamespaces } from 'react-i18next';
+import { makeStyles } from '@material-ui/styles';
+import { useTranslation } from 'react-i18next';
 
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -10,74 +10,58 @@ import { connect } from 'react-redux';
 
 import { startDevelopersViewStyle as style } from '../../assets/jss/openfin-react-doc';
 
-interface IProps extends WithStyles<typeof style>, WithNamespaces{
-
-
-}
-
 import { IRootState } from '../../reduxs';
 
 import defaultDeploySvg from '../../assets/svg/openfin-react-doc/DefaultDeploy.svg'
 import standaloneDeploySvg from '../../assets/svg/openfin-react-doc/StandaloneDeploy.svg'
 
-class StartDevelopersView extends React.Component<IProps,{}>{
-    render(){
+const useStyles = makeStyles(style);
 
-        const { classes, t } = this.props;
+const StartDevelopersView:React.FunctionComponent<{}>=(
+    props
+)=>{
 
-        return (
-            <React.Fragment>
-                <Typography variant='h3' gutterBottom>
-                    {t('developer.title')}
-                </Typography>
+    const classes = useStyles();
+    const { t, i18n } = useTranslation('start', { useSuspense: false });
 
-                <Typography variant='body1' gutterBottom>
-                    {t('developer.subTitle')}
-                </Typography>
+    return (
+        <React.Fragment>
+            <Typography variant='h3' gutterBottom>
+                {t('developer.title')}
+            </Typography>
 
-                <Typography variant='h5' gutterBottom>
-                    {t('developer.webDeployTitle')}
-                </Typography>
+            <Typography variant='body1' gutterBottom>
+                {t('developer.subTitle')}
+            </Typography>
 
-                <Paper className={classes.imgPaper}>
-                    <img src={defaultDeploySvg}/>
-                </Paper>
+            <Typography variant='h5' gutterBottom>
+                {t('developer.webDeployTitle')}
+            </Typography>
 
-                <Typography variant='body1' gutterBottom>
-                    {t('developer.webDeployDesc')}
-                </Typography>
+            <Paper className={classes.imgPaper}>
+                <img src={defaultDeploySvg}/>
+            </Paper>
 
-                <Typography className={classes.hasMargin} variant='h5' gutterBottom>
-                    {t('developer.standaloneDeployTitle')}
-                </Typography>
+            <Typography variant='body1' gutterBottom>
+                {t('developer.webDeployDesc')}
+            </Typography>
 
-                <Paper className={classes.imgPaper}>
-                    <img src={standaloneDeploySvg}/>
-                </Paper>
+            <Typography className={classes.hasMargin} variant='h5' gutterBottom>
+                {t('developer.standaloneDeployTitle')}
+            </Typography>
 
-                <Typography variant='body1' gutterBottom>
-                    {t('developer.standaloneDeployDesc')}
-                </Typography>
+            <Paper className={classes.imgPaper}>
+                <img src={standaloneDeploySvg}/>
+            </Paper>
+
+            <Typography variant='body1' gutterBottom>
+                {t('developer.standaloneDeployDesc')}
+            </Typography>
 
 
-            </React.Fragment>
-        )
-    }
+        </React.Fragment>
+    )
 }
 
-export default connect(
-    (state:IRootState)=>({
-
-    }),
-    dispatch => ({
-        actions:{
-
-        }
-    })
-
-    )(
-    withStyles(style)(
-        withNamespaces('start')(StartDevelopersView)
-    )
-);
+export default StartDevelopersView;
 
--- src/views/Start/StartFaq.tsx
+++ src/views/Start/StartFaq.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { WithStyles, withStyles } from '@material-ui/core/styles';
-import { withNamespaces, WithNamespaces } from 'react-i18next';
+import { makeStyles } from '@material-ui/styles';
+import { useTranslation } from 'react-i18next';
 
 import Typography from '@material-ui/core/Typography';
 
@@ -9,71 +9,57 @@ import { connect } from 'react-redux';
 
 import { startFaqViewStyle as style } from '../../assets/jss/openfin-react-doc';
 
-interface IProps extends WithStyles<typeof style>, WithNamespaces {
-
-
-}
-
 import { IRootState } from '../../reduxs';
 
-class StartFaqView extends React.Component<IProps,{}>{
-    render(){
-        const { classes, t } = this.props;
-        return (
-            <React.Fragment>
-                <Typography variant='h3' gutterBottom>
-                    {t('faq.title')}
-                </Typography>
-                <Typography variant='h5' gutterBottom>
-                    {t('faq.whyTs')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns1')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns2')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns3')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns4')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns5')}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    {t('faq.whyTsAns6')}
-                </Typography>
+const useStyles = makeStyles(style);
 
-                <Typography className={classes.hasMargin} variant='h5' gutterBottom>
-                    {t('faq.willResActionEmitted')}
-                </Typography>
+const StartFaqView:React.FunctionComponent<{}>=(
+    props
+)=>{
 
-                <Typography variant='body1' gutterBottom>
-                    {t('faq.willResActionEmittedAns1')}
-                </Typography>
-                <Typography variant='body1' gutterBottom>
-                    {t('faq.willResActionEmittedAns2')}
-                </Typography>
-            </React.Fragment>
-        )
-    }
+    const classes = useStyles();
+    const { t, i18n } = useTranslation('start', { useSuspense: false });
+
+    return (
+        <React.Fragment>
+            <Typography variant='h3' gutterBottom>
+                {t('faq.title')}
+            </Typography>
+            <Typography variant='h5' gutterBottom>
+                {t('faq.whyTs')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns1')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns2')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns3')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns4')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns5')}
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+                {t('faq.whyTsAns6')}
+            </Typography>
+
+            <Typography className={classes.hasMargin} variant='h5' gutterBottom>
+                {t('faq.willResActionEmitted')}
+            </Typography>
+
+            <Typography variant='body1' gutterBottom>
+                {t('faq.willResActionEmittedAns1')}
+            </Typography>
+            <Typography variant='body1' gutterBottom>
+                {t('faq.willResActionEmittedAns2')}
+            </Typography>
+        </React.Fragment>
+    )
 }
 
-export default connect(
-    (state:IRootState)=>({
-
-    }),
-    dispatch => ({
-        actions:{
-
-        }
-    })
-
-    )(
-    withStyles(style)(
-        withNamespaces('start')(StartFaqView)
-    )
-);
+export default StartFaqView;
 
